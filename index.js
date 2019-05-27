49821c7355c1f63e27bc256efead4efafdd3aae9
nasreddineskandrani <nasreddine.skandrani@gmail.com>
2019-05-27 13:10:56 -0400

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Import stylesheets
 import './style.css';
 
+import { run } from './part_1/work.js';
+
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = run;
+
---
+++ part_1/work.js
@@ -0,0 +1,7 @@
+console.log('part_1 - start');
+
+
+
+
+
+console.log('part_1 - end');
