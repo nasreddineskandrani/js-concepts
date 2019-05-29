90be0250f0237267393b741aae56e81369d0b811
nasreddineskandrani <nasreddine.skandrani@gmail.com>
2019-05-29 10:01:13 -0400

    1 file modified

--- part_1/work.js
+++ part_1/work.js
@@ -1,7 +1,91 @@
 console.log('part_1 - start');
 
+var t = 1;
+{
+  t = 2;
+}
+console.log('a :', t);
 
+var t = 1;
+{
+  let t = 2;
+}
+console.log('b :', t);
 
+var t = 1;
+{
+  var t = 2;
+}
+console.log('c :', t);
+
+var t = 1;
+{
+  const t = 2;
+}
+console.log('d :', t);
+
+var t = 1;
+function test() {
+ t = 2;
+};
+test();
+console.log('e :', t);
+
+var t = 1;
+function test() {
+ var t = 2;
+};
+test();
+console.log('f :', t);
+
+var t = 1;
+gg();
+function gg() {
+ var t = 2;
+};
+console.log('g :', t);
+
+/* vv is not a function
+var t = 1;
+vv();
+var vv = function() {
+ var t = 2;
+};
+console.log('h :', t);
+*/
+
+var yy = function() {
+ return func;
+ function func() { return 71; }
+};
+const t = yy()
+console.log('i :', t());
+
+/* Error: t is not a function
+var yy = function() {
+  var func = 33;
+  return func;
+  function func() { return 72; }
+};
+const t = yy();
+console.log('j :', t());
+*/
+
+var yy = function() {
+  return func;
+  function func() { return 73; }
+  var func = 33;
+};
+const t = yy();
+console.log('k :', t());
+
+var yy = function() {
+  return func;
+  var func = 33;
+  function func() { return 74; }
+};
+const t = yy();
+console.log('l :', t());
 
 
 console.log('part_1 - end');
